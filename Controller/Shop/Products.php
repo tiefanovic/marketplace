a1b4cc10379f65b14aa2db41b5d085fb9905447b
noh <mahmoudnoh@awstreams.com>
2017-12-04 17:20:53 +0200

    shop products

--- Controller/Shop/Products.php
+++ Controller/Shop/Products.php
@@ -2,13 +2,13 @@
 namespace AWstreams\Marketplace\Controller\Shop;
 
 
-class Rating extends \Magento\Framework\App\Action\Action
+class Products extends \Magento\Framework\App\Action\Action
 {
 
     public function execute()
     {
         $this->_view->loadLayout();
-        $this->_view->getPage()->getConfig()->getTitle()->set(__('Shop Rating'));
+       $this->_view->getPage()->getConfig()->getTitle()->set(__('Shop Products'));
         $this->_view->renderLayout();
     }
 
